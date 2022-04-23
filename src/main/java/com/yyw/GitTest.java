f15edf214d612fb3f20fc215fee06e0cc254ee55
2171231063 <73933295+2171231063@users.noreply.github.com>
2022-04-23 20:24:39 +0800

    Update GitTest.java

--- src/main/java/com/yyw/GitTest.java
+++ src/main/java/com/yyw/GitTest.java
@@ -7,5 +7,7 @@ public class GitTest {
         System.out.println("hello world");
         System.out.println("hello world");
         System.out.println("pull test");
+        
+        System.out.println("pull test");System.out.println("pull test");
     }
 }
